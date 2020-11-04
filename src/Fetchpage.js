13400770c9948c628fceaa143c215af8f23b965a
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-03 16:51:46 -0800

    made corrections

--- src/Fetchpage.js
+++ src/Fetchpage.js
@@ -5,31 +5,45 @@ export default class Fetchpage extends Component {
 
     state = {
         author: [],
-        birth-location: 'seattle, WA'
+        born: 'seattle, WA'
 
     }
 
     componentDidMount = async () => {
-        await this.fetchBirthLocation();
+        await this.fetchLocation();
     }
 
-    handleSubmit = aysnc (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
 
-        await this.fetchQuotes();
+        await this.fetchLocation();
     }
     
     handleChange = (e) => {
-        this.setState({ character: e.target.value });
+        this.setState({ author: e.target.value });
 
     }
 
     fetchBirthLocation = async () => {
         const response = await fetch.get(``)
+
+        this.setState({ born: response.body })
     }
     render() {
         return (
-            <div>
+            <div class="fetch">
+            <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} />
+                <button>Search Authors</button>
+            </form>
+                {
+                this.state.born.length === 0
+                ? 'LOADING!!'
+                : this.state.born.map( born => <div key={born.born}>
+                    <p>{born.author}</p>
+                    <p>{born.born}</p>
+                    </div>)
+                }
                 
             </div>
         )
