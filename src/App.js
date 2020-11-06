5bea9d5c8fd2967c1c18d590418f4eea6ede04ac
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-05 16:59:47 -0800

    added a route page to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
 import Fetchpage from './Fetchpage';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import ListPage from './ListPage.js';
+import DetailPage from './DetailPage.js';
 
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <ListPage {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <DetailPage {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
+}
 function App() {
   return (
     <div className="App">
